24bf51f0feb2b8bae0c3dd161afa738a374cb60e
adam.jacobson <adam.jacobson@omadahealth.com>
2025-03-31 12:45:23 -0700

    Refactor code to centralize logic for foreground and background coloring

--- lib/color_text.rb
+++ lib/color_text.rb
@@ -1,58 +1,68 @@
 class String
   BASE_COLORS = {
-    black:          30,
-    red:            31,
-    green:          32,
-    yellow:         33,
-    blue:           34,
-    magenta:        35,
-    cyan:           36,
-    white:          37,
-    bright_black:   90,
-    bright_red:     91,
-    bright_green:   92,
-    bright_yellow:  93,
-    bright_blue:    94,
-    bright_magenta: 95,
-    bright_cyan:    96,
-    bright_white:   97,
+    "black" =>          30,
+    "red" =>            31,
+    "green" =>          32,
+    "yellow" =>         33,
+    "blue" =>           34,
+    "magenta" =>        35,
+    "cyan" =>           36,
+    "white" =>          37,
+    "bright_black" =>   90,
+    "bright_red" =>     91,
+    "bright_green" =>   92,
+    "bright_yellow" =>  93,
+    "bright_blue" =>    94,
+    "bright_magenta" => 95,
+    "bright_cyan" =>    96,
+    "bright_white" =>   97,
+  }
+
+  TEXT_STYLES = {
+    "default" =>       0,
+    "bold" =>          1,
+    "dim" =>           2,
+    "italic" =>        3,
+    "underline" =>     4,
+    "inverse" =>       7,
+    "invisible" =>     8,
+    "strikethrough" => 9,
   }
 
   ANSI_CODES = {
-    default:           0,
-    bold:              1,
-    dim:               2,
-    italic:            3,
-    underline:         4,
-    inverse:           7,
-    invisible:         8,
-    strikethrough:     9,
-    on_black:          40,
-    on_red:            41,
-    on_green:          42,
-    on_yellow:         43,
-    on_blue:           44,
-    on_magenta:        45,
-    on_cyan:           46,
-    on_white:          47,
-    on_bright_black:   100,
-    on_bright_red:     101,
-    on_bright_green:   102,
-    on_bright_yellow:  103,
-    on_bright_blue:    104,
-    on_bright_magenta: 105,
-    on_bright_cyan:    106,
-    on_bright_white:   107,
-  }.merge(BASE_COLORS)
-
-  BACKGROUND_OFFSET = 10
+    "on_black" =>          40,
+    "on_red" =>            41,
+    "on_green" =>          42,
+    "on_yellow" =>         43,
+    "on_blue" =>           44,
+    "on_magenta" =>        45,
+    "on_cyan" =>           46,
+    "on_white" =>          47,
+    "on_bright_black" =>   100,
+    "on_bright_red" =>     101,
+    "on_bright_green" =>   102,
+    "on_bright_yellow" =>  103,
+    "on_bright_blue" =>    104,
+    "on_bright_magenta" => 105,
+    "on_bright_cyan" =>    106,
+    "on_bright_white" =>   107,
+  }.merge(TEXT_STYLES).merge(BASE_COLORS)
 
   START_CODE = "\e["
   END_CODE = "\e[0m"
-  BACKGROUND_256 = "48;5"
-  FOREGROUND_256 = "38;5"
-  BACKGROUND_RGB = "48;2"
-  FOREGROUND_RGB = "38;2"
+
+  BACKGROUND = {
+    "256" => "48;5",
+    "RGB" => "48;2",
+    "OFFSET" => 10,
+  }
+
+  FOREGROUND = {
+    "256" => "38;5",
+    "RGB" => "38;2",
+    "OFFSET" => 0,
+  }
+  
   INVALID_ARGUMENTS_ERROR = [
     "Invalid Arguments",
     "Must be one of the following:",
@@ -61,72 +71,30 @@ class String
     "\t- One color name as a string or symbol"
   ]
 
-  # Accepts arguments in the following forms:
-  # named colors: "".in(:red), "".in("bright_green")
-  # RGB: "".in(37, 19, 128)
-  # 256: "".in(99)
   def in(*args)
-    if args.length == 3 && args.all?(Integer)
-      ansify(FOREGROUND_RGB, *args)
-    elsif args.length == 1
-      case args[0]
-      when Integer
-        ansify(FOREGROUND_256, args[0].to_s)
-      when String, Symbol
-        if BASE_COLORS[args[0].to_sym]
-          ansify(BASE_COLORS[args[0].to_sym])
-        else
-          raise ArgumentError.new("Unrecognized color: #{args[0]}")
-        end
-      else
-        ansify(ANSI_CODES[args[0].to_sym])
-      end
-    else
-      raise ArgumentError.new(INVALID_ARGUMENTS_ERROR.join("\n"))
-    end
+    colorize_with_arguments(FOREGROUND, *args)
   end
 
-  # Accepts arguments in the following forms:
-  # named colors: "".on(:red), "".on("bright_green")
-  # RGB: "".on(37, 19, 128)
-  # 256: "".on(99)
   def on(*args)
-    if args.length == 3 && args.all?(Integer)
-      ansify(BACKGROUND_RGB, *args)
-    elsif args.length == 1
-      case args[0]
-      when Integer
-        ansify(BACKGROUND_256, args[0].to_s)
-      when String, Symbol
-        if BASE_COLORS[args[0].to_sym]
-          ansify(BASE_COLORS[args[0].to_sym] + BACKGROUND_OFFSET)
-        else
-          raise ArgumentError.new("Unrecognized color: #{args[0]}")
-        end
-      else
-        ansify(ANSI_CODES[args[0].to_sym])
-      end
-    else
-      raise ArgumentError.new(INVALID_ARGUMENTS_ERROR.join("\n"))
-    end
+    colorize_with_arguments(BACKGROUND, *args)
   end
 
   def text_color_rgb(red, green, blue)
-    ansify(FOREGROUND_RGB, red, green, blue)
+    ansify(FOREGROUND["RGB"], red, green, blue)
   end
 
   def bg_color_rgb(red, green, blue)
-    ansify(BACKGROUND_RGB, red, green, blue)
+    ansify(BACKGROUND["RGB"], red, green, blue)
   end
 
   # Set background to an arbitrary color defined by a number of 0 to 255
   def bg_color(color_code)
-    ansify(BACKGROUND_256, color_code.to_s)
+    ansify(BACKGROUND["256"], color_code.to_s)
   end
 
   # Set text to an arbitrary color defined by a number of 0 to 255
   def text_color(color_code)
-    ansify(FOREGROUND_256, color_code.to_s)
+    ansify(FOREGROUND["256"], color_code.to_s)
   end
 
   def rainbow
@@ -135,13 +103,35 @@ class String
   end
 
   def method_missing(method, *args, &block)
-    code = ANSI_CODES[method.to_sym]
+    code = ANSI_CODES[method.to_s]
     return ansify(code) if code
 
     super
   end
 
   private
+
+  def colorize_with_arguments(type, *args)
+    if args.length == 3 && args.all?(Integer)
+      ansify(type["RGB"], *args)
+    elsif args.length == 1
+      case args[0]
+      when Integer
+        ansify(type["256"], args[0].to_s)
+      when String, Symbol
+        arg = args[0].to_s
+        if BASE_COLORS[arg]
+          ansify(BASE_COLORS[arg] + type["OFFSET"])
+        else
+          raise ArgumentError.new("Unrecognized color: #{arg}")
+        end
+      else
+        ansify(ANSI_CODES[arg])
+      end
+    else
+      raise ArgumentError.new(INVALID_ARGUMENTS_ERROR.join("\n"))
+    end
+  end
 
   ENCODED_STRING_PATTERN = /^\e\[\d+(?:;\d*)*/
 
--- spec/color_text_spec.rb
+++ spec/color_text_spec.rb
@@ -7,122 +7,146 @@ def expect_codes(*codes)
   expect(yield).to eq(expected)
 end
 
-describe String do
-  let(:text) { "TEXT" }
+RSpec::Matchers.define(:have_ansi_encoding) do |text, *expected_codes|
+  combined_codes = expected_codes.map(&:to_s).join(";")
 
-  it 'uses helper method' do
-    expect_codes(31) { text.red }
+  expected = ["\e[", combined_codes, "m", text, "\e[0m"].join
+
+  match do |actual|
+    actual == expected
   end
+end
+
+describe String do
+  let(:t) { "TEXT" }
 
   it 'applies basic colors to text' do
-    expect(text.black).to          eq("\e[30mTEXT\e[0m")
-    expect(text.red).to            eq("\e[31mTEXT\e[0m")
-    expect(text.green).to          eq("\e[32mTEXT\e[0m")
-    expect(text.yellow).to         eq("\e[33mTEXT\e[0m")
-    expect(text.blue).to           eq("\e[34mTEXT\e[0m")
-    expect(text.magenta).to        eq("\e[35mTEXT\e[0m")
-    expect(text.cyan).to           eq("\e[36mTEXT\e[0m")
-    expect(text.white).to          eq("\e[37mTEXT\e[0m")
-    expect(text.bright_black).to   eq("\e[90mTEXT\e[0m")
-    expect(text.bright_red).to     eq("\e[91mTEXT\e[0m")
-    expect(text.bright_green).to   eq("\e[92mTEXT\e[0m")
-    expect(text.bright_yellow).to  eq("\e[93mTEXT\e[0m")
-    expect(text.bright_blue).to    eq("\e[94mTEXT\e[0m")
-    expect(text.bright_magenta).to eq("\e[95mTEXT\e[0m")
-    expect(text.bright_cyan).to    eq("\e[96mTEXT\e[0m")
-    expect(text.bright_white).to   eq("\e[97mTEXT\e[0m")
+    expect(t.black).to          have_ansi_encoding(t, 30)
+    expect(t.red).to            have_ansi_encoding(t, 31)
+    expect(t.green).to          have_ansi_encoding(t, 32)
+    expect(t.yellow).to         have_ansi_encoding(t, 33)
+    expect(t.blue).to           have_ansi_encoding(t, 34)
+    expect(t.magenta).to        have_ansi_encoding(t, 35)
+    expect(t.cyan).to           have_ansi_encoding(t, 36)
+    expect(t.white).to          have_ansi_encoding(t, 37)
+    expect(t.bright_black).to   have_ansi_encoding(t, 90)
+    expect(t.bright_red).to     have_ansi_encoding(t, 91)
+    expect(t.bright_green).to   have_ansi_encoding(t, 92)
+    expect(t.bright_yellow).to  have_ansi_encoding(t, 93)
+    expect(t.bright_blue).to    have_ansi_encoding(t, 94)
+    expect(t.bright_magenta).to have_ansi_encoding(t, 95)
+    expect(t.bright_cyan).to    have_ansi_encoding(t, 96)
+    expect(t.bright_white).to   have_ansi_encoding(t, 97)
   end
 
-  it 'applies basic background colors to X' do
-    expect(text.on_black).to          eq("\e[40mTEXT\e[0m")
-    expect(text.on_red).to            eq("\e[41mTEXT\e[0m")
-    expect(text.on_green).to          eq("\e[42mTEXT\e[0m")
-    expect(text.on_yellow).to         eq("\e[43mTEXT\e[0m")
-    expect(text.on_blue).to           eq("\e[44mTEXT\e[0m")
-    expect(text.on_magenta).to        eq("\e[45mTEXT\e[0m")
-    expect(text.on_cyan).to           eq("\e[46mTEXT\e[0m")
-    expect(text.on_white).to          eq("\e[47mTEXT\e[0m")
-    expect(text.on_bright_black).to   eq("\e[100mTEXT\e[0m")
-    expect(text.on_bright_red).to     eq("\e[101mTEXT\e[0m")
-    expect(text.on_bright_green).to   eq("\e[102mTEXT\e[0m")
-    expect(text.on_bright_yellow).to  eq("\e[103mTEXT\e[0m")
-    expect(text.on_bright_blue).to    eq("\e[104mTEXT\e[0m")
-    expect(text.on_bright_magenta).to eq("\e[105mTEXT\e[0m")
-    expect(text.on_bright_cyan).to    eq("\e[106mTEXT\e[0m")
-    expect(text.on_bright_white).to   eq("\e[107mTEXT\e[0m")
+  it 'applies basic background colors to text' do
+    expect(t.on_black).to          have_ansi_encoding(t, 40)
+    expect(t.on_red).to            have_ansi_encoding(t, 41)
+    expect(t.on_green).to          have_ansi_encoding(t, 42)
+    expect(t.on_yellow).to         have_ansi_encoding(t, 43)
+    expect(t.on_blue).to           have_ansi_encoding(t, 44)
+    expect(t.on_magenta).to        have_ansi_encoding(t, 45)
+    expect(t.on_cyan).to           have_ansi_encoding(t, 46)
+    expect(t.on_white).to          have_ansi_encoding(t, 47)
+    expect(t.on_bright_black).to   have_ansi_encoding(t, 100)
+    expect(t.on_bright_red).to     have_ansi_encoding(t, 101)
+    expect(t.on_bright_green).to   have_ansi_encoding(t, 102)
+    expect(t.on_bright_yellow).to  have_ansi_encoding(t, 103)
+    expect(t.on_bright_blue).to    have_ansi_encoding(t, 104)
+    expect(t.on_bright_magenta).to have_ansi_encoding(t, 105)
+    expect(t.on_bright_cyan).to    have_ansi_encoding(t, 106)
+    expect(t.on_bright_white).to   have_ansi_encoding(t, 107)
   end
 
   it 'is chainable and combines base color text with base color backgrounds' do
-    expect(text.red.on_white).to        eq("\e[31;47mTEXT\e[0m")
-    expect(text.black.on_bright_red).to eq("\e[30;101mTEXT\e[0m")
-    expect(text.magenta.on_green).to    eq("\e[35;42mTEXT\e[0m")
+    expect(t.red.on_white).to        have_ansi_encoding(t, 31, 47)
+    expect(t.black.on_bright_red).to have_ansi_encoding(t, 30, 101)
+    expect(t.magenta.on_green).to    have_ansi_encoding(t, 35, 42)
   end
 
   it 'applies text styles' do
-    expect(text.bold).to      eq("\e[1mTEXT\e[0m")
-    expect(text.underline).to eq("\e[4mTEXT\e[0m")
+    expect(t.bold).to      have_ansi_encoding(t, 1)
+    expect(t.underline).to have_ansi_encoding(t, 4)
   end
 
   describe '#in' do
+    it 'can colorize a string with a named color as symbol or string' do
+      expect(t.in(:red)).to           have_ansi_encoding(t, 31)
+      expect(t.in("green")).to        have_ansi_encoding(t, 32)
+      expect(t.in(:yellow)).to        have_ansi_encoding(t, 33)
+      expect(t.in("cyan")).to         have_ansi_encoding(t, 36)
+      expect(t.in(:bright_cyan)).to   have_ansi_encoding(t, 96)
+      expect(t.in("bright_blue")).to  have_ansi_encoding(t, 94)
+    end
+
+    # it 'can apply styles' do
+    #   expect(t.in(:bold)).to       have_ansi_encoding(t, 1)
+    #   expect(t.in("underline")).to have_ansi_encoding(t, 4)
+    # end
+
+    # it 'accepts multiple distinct basic styles' do
+    #   expect(t.in("red", :bold)).to have_ansi_encoding(t, 31;1)
+    # end
+
+    # it 'accepts multiple complex styles' do
+    #   expect(t.in("italic", [52, 144, 6])).to have_ansi_encoding(t, 3;38;2;52;144;6)
+    # end
+
     it 'can colorize a string with an arbitrary 256 color' do
-      expect(text.in(99)).to eq("\e[38;5;99mTEXT\e[0m")
+      expect(t.in(99)).to have_ansi_encoding(t, 38, 5, 99)
     end
 
     it 'can colorize a string with an arbirtary RGB code' do
-      expect(text.in(99, 15, 187)).to   eq("\e[38;2;99;15;187mTEXT\e[0m")
-      expect(text.in(0, 255, 0)).to     eq("\e[38;2;0;255;0mTEXT\e[0m")
-      expect(text.in(900, 800, 700)).to eq("\e[38;2;900;800;700mTEXT\e[0m")
-    end
-
-    it 'can colorize a string with a named color as symbol or string' do
-      expect(text.in(:red)).to           eq("\e[31mTEXT\e[0m")
-      expect(text.in("green")).to        eq("\e[32mTEXT\e[0m")
-      expect(text.in(:yellow)).to        eq("\e[33mTEXT\e[0m")
-      expect(text.in("cyan")).to         eq("\e[36mTEXT\e[0m")
-      expect(text.in(:bright_cyan)).to   eq("\e[96mTEXT\e[0m")
-      expect(text.in("bright_blue")).to  eq("\e[94mTEXT\e[0m")
+      expect(t.in(99, 15, 187)).to   have_ansi_encoding(t, 38, 2, 99, 15, 187)
+      expect(t.in(0, 255, 0)).to     have_ansi_encoding(t, 38, 2, 0, 255, 0)
+      expect(t.in(900, 800, 700)).to have_ansi_encoding(t, 38, 2, 900, 800, 700)
     end
 
     it 'raises error for unrecognized colors' do
-      expect{ text.in(:banana) }.to       raise_error(ArgumentError, /banana/)
-      expect{ text.in(:bright_flarg) }.to raise_error(ArgumentError, /bright_flarg/)
+      expect{ t.in(:banana) }.to       raise_error(ArgumentError, /banana/)
+      expect{ t.in(:bright_flarg) }.to raise_error(ArgumentError, /bright_flarg/)
     end
 
     it 'recognizes other named colors'
     # Test "orange", "brown", "indigo"
 
     it 'raises an error with invalid arguments' do
-      expect{ text.in(199, 0) }.to raise_error(ArgumentError)
+      expect{ t.in(199, 0) }.to raise_error(ArgumentError)
     end
   end
 
   describe '#on' do
     it 'can colorize a string background with an arbitrary 256 color' do
-      expect(text.on(99)).to eq("\e[48;5;99mTEXT\e[0m")
+      expect(t.on(99)).to have_ansi_encoding(t, 48, 5, 99)
     end
 
     it 'can colorize a string background with an arbirtary RGB code' do
-      expect(text.on(99, 15, 187)).to   eq("\e[48;2;99;15;187mTEXT\e[0m")
-      expect(text.on(0, 255, 0)).to     eq("\e[48;2;0;255;0mTEXT\e[0m")
-      expect(text.on(900, 800, 700)).to eq("\e[48;2;900;800;700mTEXT\e[0m")
+      expect(t.on(99, 15, 187)).to   have_ansi_encoding(t, 48, 2, 99, 15, 187)
+      expect(t.on(0, 255, 0)).to     have_ansi_encoding(t, 48, 2, 0, 255, 0)
+      expect(t.on(900, 800, 700)).to have_ansi_encoding(t, 48, 2, 900, 800, 700)
     end
 
     it 'can colorize a string background with a named color as symbol or string' do
-      expect(text.on(:red)).to           eq("\e[41mTEXT\e[0m")
-      expect(text.on("green")).to        eq("\e[42mTEXT\e[0m")
-      expect(text.on(:yellow)).to        eq("\e[43mTEXT\e[0m")
-      expect(text.on("cyan")).to         eq("\e[46mTEXT\e[0m")
-      expect(text.on(:bright_cyan)).to   eq("\e[106mTEXT\e[0m")
-      expect(text.on("bright_blue")).to  eq("\e[104mTEXT\e[0m")
+      expect(t.on(:red)).to           have_ansi_encoding(t, 41)
+      expect(t.on("green")).to        have_ansi_encoding(t, 42)
+      expect(t.on(:yellow)).to        have_ansi_encoding(t, 43)
+      expect(t.on("cyan")).to         have_ansi_encoding(t, 46)
+      expect(t.on(:bright_cyan)).to   have_ansi_encoding(t, 106)
+      expect(t.on("bright_blue")).to  have_ansi_encoding(t, 104)
     end
 
     it 'raises error for unrecognized colors' do
-      expect{ text.on(:banana) }.to       raise_error(ArgumentError, /banana/)
-      expect{ text.on(:bright_flarg) }.to raise_error(ArgumentError, /bright_flarg/)
+      expect{ t.on(:banana) }.to       raise_error(ArgumentError, /banana/)
+      expect{ t.on(:bright_flarg) }.to raise_error(ArgumentError, /bright_flarg/)
+    end
+
+    it 'raises error for invalid backgrounds' do
+      expect{ t.on(:bold) }.to raise_error(ArgumentError)
+      expect{ t.on(:italic) }.to raise_error(ArgumentError)
     end
 
     it 'raises an error with invalid arguments' do
-      expect{ text.on(199, 0) }.to raise_error(ArgumentError)
+      expect{ t.on(199, 0) }.to raise_error(ArgumentError)
     end
   end
 
@@ -132,46 +156,46 @@ describe String do
 
   describe "#rainbow" do
     it "colorizes each character" do
-      expect(text.rainbow).to match(/(\e\[\d{2}m\w{1}\e\[0m)+/)
+      expect(t.rainbow).to match(/(\e\[\d{2}m\w{1}\e\[0m)+/)
     end
   end
 
   describe '#text_color' do
     it 'can colorize a string with an arbitrary color' do
-      expect(text.text_color(99)).to eq("\e[38;5;99mTEXT\e[0m")
+      expect(t.text_color(99)).to have_ansi_encoding(t, 38, 5, 99)
     end
   end
 
   describe '#bg_color' do
     it 'can colorize a string with an arbitrary color' do
-      expect(text.bg_color(99)).to eq("\e[48;5;99mTEXT\e[0m")
+      expect(t.bg_color(99)).to have_ansi_encoding(t, 48, 5, 99)
     end
   end
 
   describe '#text_color_rgb' do
     it 'can colorize a string with an arbirtary RGB code' do
-      expect(text.text_color_rgb(99, 15, 187)).to   eq("\e[38;2;99;15;187mTEXT\e[0m")
-      expect(text.text_color_rgb(0, 255, 0)).to     eq("\e[38;2;0;255;0mTEXT\e[0m")
-      expect(text.text_color_rgb(900, 800, 700)).to eq("\e[38;2;900;800;700mTEXT\e[0m")
+      expect(t.text_color_rgb(99, 15, 187)).to   have_ansi_encoding(t, 38, 2, 99, 15, 187)
+      expect(t.text_color_rgb(0, 255, 0)).to     have_ansi_encoding(t, 38, 2, 0, 255, 0)
+      expect(t.text_color_rgb(900, 800, 700)).to have_ansi_encoding(t, 38, 2, 900, 800, 700)
     end
   end
 
   describe '#bg_color_rgb' do
     it 'can colorize a string with an arbirtary RGB code' do
-      expect(text.bg_color_rgb(99, 15, 187)).to   eq("\e[48;2;99;15;187mTEXT\e[0m")
-      expect(text.bg_color_rgb(0, 255, 0)).to     eq("\e[48;2;0;255;0mTEXT\e[0m")
-      expect(text.bg_color_rgb(900, 800, 700)).to eq("\e[48;2;900;800;700mTEXT\e[0m")
+      expect(t.bg_color_rgb(99, 15, 187)).to   have_ansi_encoding(t, 48, 2, 99, 15, 187)
+      expect(t.bg_color_rgb(0, 255, 0)).to     have_ansi_encoding(t, 48, 2, 0, 255, 0)
+      expect(t.bg_color_rgb(900, 800, 700)).to have_ansi_encoding(t, 48, 2, 900, 800, 700)
     end
 
     it 'can be chained' do
-      expect(text.bg_color_rgb(99, 15, 187).bold).to eq("\e[48;2;99;15;187;1mTEXT\e[0m")
+      expect(t.bg_color_rgb(99, 15, 187).bold).to have_ansi_encoding(t, 48, 2, 99, 15, 187, 1)
     end
   end
 
   describe 'combining effects' do
     it 'combines code additively' do
-      string = text.text_color(99).bg_color(127)
-      expect(string).to eq("\e[38;5;99;48;5;127mTEXT\e[0m")
+      string = t.text_color(99).bg_color(127)
+      expect(string).to have_ansi_encoding(t, 38, 5, 99, 48, 5, 127)
     end
   end
 end
